61911454a1dabed01e395831e27d6d83ad7366b4
mrdavidalv <davidalvarez33@hotmail.com>
2025-05-17 00:04:46 -0500

    fullRobot

---
+++ src/axioma_nav2/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+from glob import glob
+
+package_name = 'axioma_nav2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.py')),
+        (os.path.join('share', package_name), glob('worlds/*.world.xml')),
+  	    ('share/' + package_name+'/param/', glob('param/*')),
+  	    ('share/' + package_name+'/rviz/', glob('rviz/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot',
+    maintainer_email='olmerg@gmail.com',
+    description='Paquete que sirve de brigde de nav2',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
